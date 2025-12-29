7a36eb37ddcd192ee500b7f3dfe9d18f41d6dcf0
victoroladejo78 <victoroladejo78@gmail.com>
2025-12-29 09:53:13 +0100

    Update and rename bui sen.js to indexx.html

--- OLADEJO VICTOR Website BUI-SEN 313/bui sen.js
+++
@@ -1,7 +0,0 @@
-function toggleTheme() {
-    document.body.classList.toggle("dark-mode");
-}
-
-const yearElement = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-yearElement.textContent = `© ${currentYear} Victor Oladejo`;
